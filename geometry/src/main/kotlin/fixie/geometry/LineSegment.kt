7acf7db10fe0d9deb939df9f43dd183bbc0816f9
knokko <knokogator@hotmail.com>
2024-02-16 14:36:46 +0100

    Add conveniences to LineSegment

--- geometry/src/main/kotlin/fixie/geometry/LineSegment.kt
+++ geometry/src/main/kotlin/fixie/geometry/LineSegment.kt
@@ -1,6 +1,6 @@
 package fixie.geometry
 
-import fixie.Displacement
+import fixie.*
 
 /**
  * Represents a line segment from (startX, startY) to (startX + lengthX, startY + lengthY)
@@ -11,5 +11,19 @@ class LineSegment(
     val lengthX: Displacement,
     val lengthY: Displacement
 ) {
+
+    val minX: Displacement
+        get() = min(startX, startX + lengthX)
+    val minY: Displacement
+        get() = min(startY, startY + lengthY)
+    val maxX: Displacement
+        get() = max(startX, startX + lengthX)
+    val maxY: Displacement
+        get() = max(startY, startY + lengthY)
+
     override fun toString() = "LineSegment($startX, $startY, $lengthX, $lengthY)"
+
+    fun overlapsBounds(minX: Displacement, minY: Displacement, maxX: Displacement, maxY: Displacement): Boolean {
+        return this.minX <= maxX && this.minY <= maxY && this.maxX >= minX && this.maxY >= minY
+    }
 }

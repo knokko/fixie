3a4e40578df054178ad729b4f77dd670b9b908a4
knokko <knokogator@hotmail.com>
2024-06-25 18:20:24 +0200

    Fix some indentation

--- physics-kit/src/main/kotlin/fixie/AngleUnit.kt
+++ physics-kit/src/main/kotlin/fixie/AngleUnit.kt
@@ -1,6 +1,6 @@
 package fixie
 
 enum class AngleUnit(val suffix: String, val maxValue: Double) {
-    DEGREES("°", 360.0),
-    RADIANS("rad", 6.283185307179586);
+	DEGREES("°", 360.0),
+	RADIANS("rad", 6.283185307179586);
 }
